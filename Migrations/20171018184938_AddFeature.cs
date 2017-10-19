c5d9395cdb5b8f77536e29fdbd95857a673d9098
tunjidev6 <tunjia@dev6.com>
2017-10-19 11:48:31 -0400

    add features

---
+++ Migrations/20171018184938_AddFeature.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace vega.Migrations
+{
+    public partial class AddFeature : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Models_Makes_MakeId",
+                table: "Models");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "MakeId",
+                table: "Models",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Features",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Features", x => x.Id);
+                });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Models_Makes_MakeId",
+                table: "Models",
+                column: "MakeId",
+                principalTable: "Makes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Models_Makes_MakeId",
+                table: "Models");
+
+            migrationBuilder.DropTable(
+                name: "Features");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "MakeId",
+                table: "Models",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Models_Makes_MakeId",
+                table: "Models",
+                column: "MakeId",
+                principalTable: "Makes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
